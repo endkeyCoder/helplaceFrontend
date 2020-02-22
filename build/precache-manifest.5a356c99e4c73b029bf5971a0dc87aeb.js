10b0b49c523c7f23b1ef98590c1e54955bbbfb3b
endkeyCoder <helplace2019@gmail.com>
2020-02-22 13:08:39 -0300

    adicao da pasta build

---
+++ build/precache-manifest.5a356c99e4c73b029bf5971a0dc87aeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3cbb0e662a71f6040a1491038ec90e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "205fd6d813319b22b245",
+    "url": "/static/css/main.b344656a.chunk.css"
+  },
+  {
+    "revision": "80f97e050523e0ab3b82",
+    "url": "/static/js/2.f80c8474.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.f80c8474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205fd6d813319b22b245",
+    "url": "/static/js/main.3be20f66.chunk.js"
+  },
+  {
+    "revision": "38d718477c5cf02ffe61",
+    "url": "/static/js/runtime-main.a3cb67ef.js"
+  },
+  {
+    "revision": "8b34cea799c039db653ae976d9dc3039",
+    "url": "/static/media/bgSectionHome3.8b34cea7.png"
+  }
+]);
